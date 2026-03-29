e9e67e128503a6fbdb1919cbc184487e058657af
aanushkasaha <sahaaanushka28@gmail.com>
2026-03-29 16:26:53 +0530

    hero section.

---
+++ components/HeroSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className="py-16">
+        <h1 className="text-[104px] font-extrabold uppercase leading-[110px] tracking-tight">
+            Building the <br /> Future of <br /> Technology
+        </h1>
+        <p className="mt-6 text-[20px] font-normal leading-[144%] text-gray-400">
+            The Student Technical Council at IIT Patna is the apex body of all
+            technical activities on campus. We foster innovation, organize technical
+            events, manage clubs, and provide a platform for students to showcase
+            their talents. From workshops to hackathons, from seminars to
+            competitions, STC is at the forefront of technical excellence at IIT
+            Patna, empowering students to learn, create, and innovate.
+        </p>
+    </section>
+  );
+}
+
+export default HeroSection;
